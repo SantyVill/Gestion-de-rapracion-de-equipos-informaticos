36f36484c71a76b0974ae63806b40d76ef35dad8
GonzaloMY <mgy.yustos@gmail.com>
2022-01-08 00:19:40 -0300

    Revert "cambio en home.blade.php"
    This reverts commit 2356c1193e9776e5a1e5e947392a0c6635f9d6a0.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,5 @@
 @section('titulo','Home')
 
 @section('contenido')
-<<<<<<< HEAD
     <h1>Esta es la página de home de mario.</h1>
-=======
-    <h1>Esta es la página de home de Santiago.</h1>
->>>>>>> parent of a46b3b5 (palabra home original)
 @endsection
