001fcf820ebf0319844f74d405c362b615892976
GonzaloMY <mgy.yustos@gmail.com>
2022-03-13 18:05:05 -0300

    Completados los Models Recepcion y Revision

--- app/Models/Recepcion.php
+++ app/Models/Recepcion.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Recepcion extends Model
 {
+    protected $table = 'recepciones';
+    protected $fillable = ['falla','accesorio','fecha_recepcion','fecha_entrega','informe_final','observacion','precio','garantia'];
+    protected $hidden = ['create_at','update_at'];
     use HasFactory;
+    public function cliente(){
+        return $this->belongsTo('App\Models\Cliente');
+    }
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+    public function equipo(){
+        return $this->belongsTo('App\Models\Equipo');
+    }
+    public function estado(){
+        return $this->belongsTo('App\Models\Estado');
+    }
 }
