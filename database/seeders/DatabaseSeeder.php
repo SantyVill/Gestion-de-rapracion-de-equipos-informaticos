edb5bb68a417fc59ff6994097a4f0b2b8419b359
SantyVillagra <santyvillagra@gmail.com>
2022-04-09 17:57:36 -0300

    agrego datos al seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Rol;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        
+        Rol::create(['rol'=>'admin']);
+        Rol::create(['rol'=>'tecnico']);
+        Rol::create(['rol'=>'recepcionista']);
+        
+        User::create(['apellido'=>'administrador',
+                        'nombre'=>'admin',
+                        'email'=>'admin@admin.com',
+                        'password'=>'12345']);
+
+        DB::table('rol_user')->insert([
+            'user_id'=>1,
+            'rol_id'=>1
+        ]);
     }
 }
